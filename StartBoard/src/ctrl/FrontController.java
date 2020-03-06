e3aa2d038da23271b1971806b535d5c6fcc7688e
bbster <bbster12@naver.com>
2020-03-06 18:17:56 +0900

    Logger 라는 라이브러리 사용 Log 찍기

--- StartBoard/src/ctrl/FrontController.java
+++ StartBoard/src/ctrl/FrontController.java
@@ -1,4 +1,5 @@
 package ctrl;
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -7,41 +8,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FrontController extends HttpServlet{
-	
-	public FrontController() {}
-	
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class FrontController extends HttpServlet {
+	private static Logger log = LoggerFactory.getLogger(FrontController.class);
+
+	public FrontController() {
+	}
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String uri = request.getRequestURI(); // Domain 뒤 url 주소 모두 긁어옴
-		System.out.println("uri: " + uri);
-		
+		log.info("uri: " + uri);
+
 		String contextPath = request.getContextPath(); // Domain 뒤 API 까지의 Location
-		System.out.println("context: " + contextPath);
-		
-		String path = uri.substring(contextPath.length());  // 실제로 작동하는 API Location
-		System.out.println("path: " + path);
-		
-		String targetPage = "" ; // targetPage 값을 null값으로 두고 아래 if문을 통해 조건별로 targetPage 변경
-		
-		if(path.equals("/test.do")) {  // 
-			String name = request.getParameter("name");  // GET방식으로 넘어온 파라미터값 받아와서 command 출력
-			System.out.println("name: " + name);
+		log.info("context: " + contextPath);
+
+		String path = uri.substring(contextPath.length()); // 실제로 작동하는 API Location
+		log.info("path: " + path);
+
+		String targetPage = ""; // targetPage 값을 null값으로 두고 아래 if문을 통해 조건별로 targetPage 변경
+
+		if (path.equals("/test.do")) { //
+			String name = request.getParameter("name"); // GET방식으로 넘어온 파라미터값 받아와서 command 출력
+			log.info("name: " + name);
 			targetPage = "/testReturn.jsp";
 		}
-		
+
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(targetPage); // targetPage로 보내준다
 		requestDispatcher.forward(request, response); // data binding
-	} 
-	
+	}
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		service(request, response);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		service(request, response);
 	}
 }
